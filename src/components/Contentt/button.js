a3cb275c0feef23587b76e0be9ea66f21e853e9c
Jambalsuren0012 <naruto_0212@yahoo.com>
2023-01-04 17:17:02 +0800

    up

---
+++ src/components/Contentt/button.js
@@ -0,0 +1,13 @@
+const Btn = (props) => {
+    const HandlleClick = (name) => {
+        alert("Hello-" + name);
+
+    };
+    return(
+        <button
+        className="{props.classNer}">
+        
+
+        </button>
+    )
+}
--- src/components/Navbar/Head.js
+++ src/components/Navbar/Head.js
@@ -8,7 +8,7 @@ const Head = () => {
         <h2 className="blogh2">I think so, this is it.</h2>
         <p>Design begins after I begin to think about how to present an experience most successfully, whether a button I put in can solve a problem. The only point in design is not ui design, if the user does not have a good experience at the end of the product, the design will be considered unsuccessful in my opinion.</p>
         </div>
-           <img src="./images/Saly-16.png" alt="" />
+           <img className="Saly" src="./images/Saly-16.png" alt="" />
        </div>
     </div>;
 };
