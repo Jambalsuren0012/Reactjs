a6b1b2c0c6cb328b9379a3e5a5f4fb4ed30863e0
Jambalsuren0012 <naruto_0212@yahoo.com>
2023-01-09 14:16:58 +0800

    up

--- src/components/Contentt/button.js
+++ src/components/Contentt/button.js
@@ -1,13 +1,20 @@
-const Btn = (props) => {
-    const HandlleClick = (name) => {
-        alert("Hello-" + name);
+// const Btn = (props) => {
+//     const HandlleClick = (name) => {
+//         alert("Hello-" + name);
 
-    };
-    return(
-        <button
-        className="{props.classNer}">
+//     };
+//     return(
+//         <button
+//         className="{props.classNer}">
         
 
-        </button>
-    )
+//         </button>
+//     )
+// }
+import { Button } from "bootstrap"
+const button =() => {
+    return 
+    <div>
+        <button></button>
+    </div>
 }
